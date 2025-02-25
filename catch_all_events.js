ba73b798bb5b7e9b5592b4c762bec182df7317be
tony <tony.garhager@gmail.com>
2025-02-25 06:59:01 -0800

    filter events with token address

--- catch_all_events.js
+++ catch_all_events.js
@@ -1,24 +1,24 @@
 require("dotenv").config();
 const { ethers } = require("ethers");
-const retry = require("retry");
+//const { MongoClient } = require("mongodb");
 
 // Load environment variables
 const WS_URL = process.env.WS_URL;
 const CONTRACT_ABI = [
     "event Transfer(address indexed from, address indexed to, uint value)",
-    "event Approval(address indexed owner, address indexed spender, uint value)",
-    /*
-    "event Pause()",
-    "event Unpause()",
-    "event DestroyedBlackFunds(address _blackListedUser, uint _balance)",
-    "event AddedBlackList(address _user)",
-    "event RemovedBlackList(address _user)",
-    "event Issue(uint amount)",
-    "event Redeem(uint amount)",
-    "event Deprecate(address newAddress)",
-    "event Params(uint feeBasisPoints, uint maxFee)"
-    */
+    // "event Approval(address indexed owner, address indexed spender, uint value)",
+    // "event Pause()",
+    // "event Unpause()",
+    // "event DestroyedBlackFunds(address _blackListedUser, uint _balance)",
+    // "event AddedBlackList(address _user)",
+    // "event RemovedBlackList(address _user)",
+    // "event Issue(uint amount)",
+    // "event Redeem(uint amount)",
+    // "event Deprecate(address newAddress)",
+    // "event Params(uint feeBasisPoints, uint maxFee)"
 ];
+
+const TOKEN_ADDR = process.env.TOKEN_ADDRESS;
 
 // Create an Interface for decoding USDT events
 const contractInterface = new ethers.Interface(CONTRACT_ABI);
@@ -33,7 +33,10 @@ async function watchBlocks() {
     console.log("🔗 Connecting to Ethereum WebSocket...");
 
     let provider = createProvider();
-    const dburi = "mongodb+srv://tonygarhager:kkndkknd823@cluster0.k3tc3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    // const dburi = "mongodb+srv://tonygarhager:kkndkknd823@cluster0.k3tc3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    // const dbClient = new MongoClient(dburi);
+    // await dbClient.connect();
+    
     // Listen for new blocks
     provider.on("block", async (blockNumber) => {
         console.log(`⛓  New Block: #${blockNumber}`);
@@ -49,24 +52,37 @@ async function watchBlocks() {
                     // Get transaction receipt (to check events)
                     const receipt = await provider.getTransactionReceipt(tx);
                     if (receipt && receipt.logs.length > 0) {
-                        console.log(`🔍 Tx Hash: ${tx}`);
+                        
+                        let first = true;
 
                         // Enumerate all events (logs)
                         receipt.logs.forEach((log, index) => {
-                            try {
-                                const parsedLog = contractInterface.parseLog(log);
-                                console.log(`📢 Event: ${parsedLog.name}`);
-                                console.log(`   - Contract Address: ${log.address}`);
-                                console.log(`   - Topics: ${JSON.stringify(log.topics)}`);
-                                console.log(`   - Data: ${log.data}`);
-                                console.log("--------------------------------------");
-                            } catch (error) {
-                                /*console.log(`📢 Event: ${log.index + 1}`);
-                                console.log(`   - Contract Address: ${log.address}`);
-                                console.log(`   - Topics: ${JSON.stringify(log.topics)}`);
-                                console.log(`   - Data: ${log.data}`);
-                                console.log("--------------------------------------");
-                                */
+                            if (log.address.toLowerCase() === TOKEN_ADDR.toLowerCase()) {
+                                try {
+
+                                    const parsedLog = contractInterface.parseLog(log);
+                                    
+                                    if (first) {
+                                        console.log(`🔍 Tx Hash: ${tx}`);
+                                        first = false;
+                                    }
+                                    console.log(`📢 Event: ${parsedLog.name}`);
+                                    console.log(`   - From: ${parsedLog.args.from}`);
+                                    console.log(`   - To: ${parsedLog.args.to}`);
+                                    console.log(`   - Value: ${ethers.formatUnits(parsedLog.args.value, 6)} USDT`);
+
+                                    // console.log(`   - Contract Address: ${log.address}`);
+                                    // console.log(`   - Topics: ${JSON.stringify(log.topics)}`);
+                                    // console.log(`   - Data: ${log.data}`);
+                                    console.log("--------------------------------------");
+                                } catch (error) {
+                                    /*console.log(`📢 Event: ${log.index + 1}`);
+                                    console.log(`   - Contract Address: ${log.address}`);
+                                    console.log(`   - Topics: ${JSON.stringify(log.topics)}`);
+                                    console.log(`   - Data: ${log.data}`);
+                                    console.log("--------------------------------------");
+                                    */
+                                }
                             }
                     });
                     }
